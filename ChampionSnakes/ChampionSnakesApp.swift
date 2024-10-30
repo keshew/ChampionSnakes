d3b0f3c0a66ccec075b152efc6aa16bc42ff31cb
keshew <artyom.korotckov2017@yandex.ru>
2024-10-31 01:04:45 +0500

    Initial Commit

---
+++ ChampionSnakes/ChampionSnakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChampionSnakesApp.swift
+//  ChampionSnakes
+//
+//  Created by Артём Коротков on 31.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChampionSnakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
